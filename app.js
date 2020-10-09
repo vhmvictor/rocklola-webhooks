b3ed0d78d3aa7ff2bfaf5243cbc02d4732e08eaf
Victor <victor.hugomarques@hotmail.com>
2020-10-09 11:33:58 -0300

    update app.js

--- app.js
+++ app.js
@@ -21,11 +21,74 @@ app.post("/order_created_hook", async (req, res, next) => {
           access_token: process.env.ACCESS_TOKEN,
           user_agent: process.env.USER_AGENT
         });
-        let order = await nuvemshop.getOrder(req.body.id);
+        let order = [];
+        order.push(await nuvemshop.getOrder(req.body.id));
         console.log(order);
-        res.send(order);
+        let created_lead = [];
+        order.forEach((obj) => {
+            created_lead.push({
+                "event_type": "Compra Pendente",
+                "event_family":"CDP",
+                "payload": {
+                  "conversion_identifier": "OPPORTUNITY",
+                  "name": obj.name,
+                  "email": obj.email,
+                  "job_title": "",
+                  "state": obj.default_address.province,
+                  "city": obj.default_address.city,
+                  "country": obj.default_address.country,
+                  "personal_phone": "",
+                  "mobile_phone": obj.phone,
+                  "twitter": "",
+                  "facebook": "",
+                  "linkedin": "",
+                  "website": "",
+                  "cf_custom_field_api_identifier": JSON.stringify(obj.identification),
+                  "company_name": "",
+                  "company_site": "",
+                  "company_address": "",
+                  "client_tracking_id": JSON.stringify(obj.id),
+                  "traffic_source": "Nuvemshop",
+                  "traffic_medium": "",
+                  "traffic_campaign": "",
+                  "traffic_value": "",
+                  "tags": ["nuvemshop", "api", "nani"],
+                  "available_for_mailing": true,
+                  "legal_bases": [
+                    {
+                      "category": "data_processing",
+                      "type": "pre_existent_contract"
+                    },
+                    {
+                      "category": "communications",
+                      "type": "consent",
+                      "status": "granted"
+                    }
+                  ]
+                }
+              });
+        });
+        let authentication = await axios({ method: 'POST', url: process.env.RD_URL,
+          data: {
+            client_id: process.env.RD_CLIENT_ID,
+            client_secret: process.env.RD_CLIENT_SECRET,
+            refresh_token: process.env.RD_REFLESH_TOKEN
+          }
+        });
+        //
+        created_lead.forEach(async (item) => {
+            console.log(item)
+            let request = await axios({ method: 'POST', url: process.env.RD_url,
+              headers: {
+                Authorization: "Bearer" + authentication.access_token
+              },
+              data: item
+            });
+          });
+        console.log(request.data);
+        return res.status(200).send('Order criada com sucesso.');
     } catch (error) {
-        console.log(error.response.data);
+        return res.status(500).send('Erro ao criar a order.');
     }
 })
 //
--- teste.js
+++ teste.js
@@ -11,64 +11,57 @@ const app = async () => {
         access_token: process.env.ACCESS_TOKEN,
         user_agent: process.env.USER_AGENT
       });
-      const orders = await nuvemshop.getOrder(304430247);
-      console.log(orders)
-      let pending_orders = [];
-      let paid_orders = [];
-      let cont = 0;
+      let order = []
+      order.push(await nuvemshop.getOrder(304430247));
+      console.log(order)
       //
       orders.forEach((obj) => {
-        if(obj.status == "open" && obj.payment_status == "pending" && obj.created_at > "2020-10-08") {
-          cont ++
-          //console.log(obj)
-          /*
-          pending_orders.push({
-            "event_type": "CONVERSION",
-            "event_family":"CDP",
-            "payload": {
-              "conversion_identifier": "Nuvemshop Lead",
-              "name": obj.name,
-              "email": obj.email,
-              "job_title": "",
-              "state": obj.default_address.province,
-              "city": obj.default_address.city,
-              "country": obj.default_address.country,
-              "personal_phone": "",
-              "mobile_phone": obj.phone,
-              "twitter": "",
-              "facebook": "",
-              "linkedin": "",
-              "website": "",
-              "cf_custom_field_api_identifier": JSON.stringify(obj.identification),
-              "company_name": "",
-              "company_site": "",
-              "company_address": "",
-              "client_tracking_id": JSON.stringify(obj.id),
-              "traffic_source": "Nuvemshop",
-              "traffic_medium": "",
-              "traffic_campaign": "",
-              "traffic_value": "",
-              "tags": ["nuvemshop", "api", "nani"],
-              "available_for_mailing": true,
-              "legal_bases": [
-                {
-                  "category": "data_processing",
-                  "type": "pre_existent_contract"
-                },
-                {
-                  "category": "communications",
-                  "type": "consent",
-                  "status": "granted"
-                }
-              ]
-            }
-          });
-          */
-        }
+        /*
+        pending_orders.push({
+          "event_type": "CONVERSION",
+          "event_family":"CDP",
+          "payload": {
+            "conversion_identifier": "Nuvemshop Lead",
+            "name": obj.name,
+            "email": obj.email,
+            "job_title": "",
+            "state": obj.default_address.province,
+            "city": obj.default_address.city,
+            "country": obj.default_address.country,
+            "personal_phone": "",
+            "mobile_phone": obj.phone,
+            "twitter": "",
+            "facebook": "",
+            "linkedin": "",
+            "website": "",
+            "cf_custom_field_api_identifier": JSON.stringify(obj.identification),
+            "company_name": "",
+            "company_site": "",
+            "company_address": "",
+            "client_tracking_id": JSON.stringify(obj.id),
+            "traffic_source": "Nuvemshop",
+            "traffic_medium": "",
+            "traffic_campaign": "",
+            "traffic_value": "",
+            "tags": ["nuvemshop", "api", "nani"],
+            "available_for_mailing": true,
+            "legal_bases": [
+              {
+                "category": "data_processing",
+                "type": "pre_existent_contract"
+              },
+              {
+                "category": "communications",
+                "type": "consent",
+                "status": "granted"
+              }
+            ]
+          }
+        });
+        */
       });
       console.log(cont)
 
-     /*
       object.forEach(async (item) => {
         console.log(item)
         let request = await axios({ method: 'POST', url: process.env.URL_RD,
@@ -79,7 +72,7 @@ const app = async () => {
         });
         console.log(request.data);
       });
-      */
+      
     } catch (error) {
         console.log(error.response.data);
     }    
