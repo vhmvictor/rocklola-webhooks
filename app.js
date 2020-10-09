94ff25e76911893960034e376551ed986c14cf1c
Victor <victor.hugomarques@hotmail.com>
2020-10-09 09:04:31 -0300

    fist commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+const nuvemshop = require("nuvemshop");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+//TESTE DO SERVIDOR
+app.get("/hello", (request, response) => {
+    response.json({ message: 'Hello Rocklola' })
+})
---
+++ teste.js
@@ -0,0 +1,86 @@
+const axios = require("axios");
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = async () => {
+    try {
+      nuvemshop.config({
+        store_id: process.env.STORE_ID,
+        access_token: process.env.ACCESS_TOKEN,
+        user_agent: process.env.USER_AGENT
+      });
+      const orders = await nuvemshop.listOrders();
+      let pending_orders = [];
+      let paid_orders = [];
+      let cont = 0;
+      //
+      orders.forEach((obj) => {
+        if(obj.status == "open" && obj.payment_status == "pending" && obj.created_at > "2020-10-08") {
+          cont ++
+          console.log(obj)
+          /*
+          pending_orders.push({
+            "event_type": "CONVERSION",
+            "event_family":"CDP",
+            "payload": {
+              "conversion_identifier": "Nuvemshop Lead",
+              "name": obj.name,
+              "email": obj.email,
+              "job_title": "",
+              "state": obj.default_address.province,
+              "city": obj.default_address.city,
+              "country": obj.default_address.country,
+              "personal_phone": "",
+              "mobile_phone": obj.phone,
+              "twitter": "",
+              "facebook": "",
+              "linkedin": "",
+              "website": "",
+              "cf_custom_field_api_identifier": JSON.stringify(obj.identification),
+              "company_name": "",
+              "company_site": "",
+              "company_address": "",
+              "client_tracking_id": JSON.stringify(obj.id),
+              "traffic_source": "Nuvemshop",
+              "traffic_medium": "",
+              "traffic_campaign": "",
+              "traffic_value": "",
+              "tags": ["nuvemshop", "api", "nani"],
+              "available_for_mailing": true,
+              "legal_bases": [
+                {
+                  "category": "data_processing",
+                  "type": "pre_existent_contract"
+                },
+                {
+                  "category": "communications",
+                  "type": "consent",
+                  "status": "granted"
+                }
+              ]
+            }
+          });
+          */
+        }
+      });
+      console.log(cont)
+
+     /*
+      object.forEach(async (item) => {
+        console.log(item)
+        let request = await axios({ method: 'POST', url: process.env.URL_RD,
+          headers: {
+            Authorization: process.env.TOKEN_RD
+          },
+          data: item
+        });
+        console.log(request.data);
+      });
+      */
+    } catch (error) {
+        console.log(error.response.data);
+    }    
+}
+//
+app();
